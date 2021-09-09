151625d6a5391bda56fb1cda5a24fc804592d65e
Dunstan BECHT <dunstan@becht.network>
2021-09-09 17:29:59 +0200

    update tests

--- tests/test_analyze.py
+++ tests/test_analyze.py
@@ -12,11 +12,18 @@ stm = '10_rho5e14m-2_square_2000nm_RDD_d5e-4nm-2_edge_PBCR2_S0'
 ttl = r"10 RDD $ \left( d = 5 \times 10^{-4} \mathrm{nm^{-2}} \right) $"
 
 # test output_data
-o = analyze.output_data(stm, 'data')
-print(o['stm'])
+outdat = analyze.output_data(stm, impdir='data')
+print(outdat['stm'])
 print()
 
 # test exoport
-analyze.export(stm, 'data', 'fits', ttl, exfmto='svg', exfmtf='svg')
+analyze.export(
+    stm,
+    impdir='data',
+    expdir='fits',
+    title=ttl,
+    fmtout='svg',
+    fmtfit='svg',
+)
 
 input("OK")
--- tests/test_collect.py
+++ tests/test_collect.py
@@ -8,19 +8,19 @@ Script to test the module collect.
 from lpa.output import collect
 
 output = '10_rho5e14m-2_square_2000nm_RDD_d5e-4nm-2_edge_PBCR2_S0'
-q = ['n', 's']
+qtynam = ('n', 's')
 
 # load file
-print(collect.load_file(q, '01', 'data/'+output))
+print(collect.load_file(qtynam, '01', impdir='data/'+output))
 print()
 
 # load directory
-print(collect.load_directory(q, output, 'data'))
+print(collect.load_directory(qtynam, output, impdir='data'))
 print()
 
 # versatile load
-print(collect.load(q, output, 'data'))
-print(collect.load(q, '01.dat', 'data/'+output))
+print(collect.load(qtynam, output, impdir='data'))
+print(collect.load(qtynam, '01.dat', impdir='data/'+output))
 print()
 
 input("OK")
