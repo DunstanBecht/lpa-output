caeec0b16e8abf979b13021d39b47a1b1aba5149
Dunstan BECHT <dunstan@becht.network>
2021-11-15 10:01:53 +0100

    update tests

--- tests/test_collect.py
+++ tests/test_collect.py
@@ -9,7 +9,7 @@ from lpa.output import collect
 
 outfil = 'rho5e13m-2_square_3200nm_RDD_d5e-5nm-2_screw_PBC1_S0_output'
 outdir = '10_rho5e13m-2_square_3200nm_RDD_d5e-5nm-2_screw_S0_PBC1_output'
-qtynam = ('n', 's', 'd')
+qtynam = ('v', 'nd', 's', 'd')
 
 # load file
 print(collect.load_file(qtynam, outfil, impdir='data'))
