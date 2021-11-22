737720c0711c73e1dffcd25d666ffcb23ec2a8b3
Dunstan BECHT <dunstan@becht.network>
2021-11-22 15:35:21 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+"""
+Although pyproject.toml is the new unified Python project settings file
+that replaces setup.py, setup.py is needed for editable installations.
+"""
+
+from setuptools import setup
+
+setup()
