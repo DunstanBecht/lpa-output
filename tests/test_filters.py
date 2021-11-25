6a8a310af33dfa4b833e4e82e90909c980c57c3f
Dunstan BECHT <dunstan@becht.network>
2021-11-25 12:45:24 +0100

    update examples

--- tests/test_filters.py
+++ tests/test_filters.py
@@ -16,10 +16,10 @@ for a in [
     np.array([9, 8, 7, 6, 5, 4, 4, 2, 1, 0]),
 ]:
     l = np.arange(len(a))
-    i1 = filters.f1(a)
+    i1 = filters.F1(a)
     plt.plot(l, a, "o-", label="all")
     plt.plot(l[:i1], a[:i1], ".-", label="filtered")
-    plt.title("f1")
+    plt.title("F1")
     plt.legend()
     plt.show()
         
@@ -29,10 +29,10 @@ for y in [
     np.array([0.0, 1.0, 2.0, 3.0, 4.0, 5.2, 6.8, 8.6, 11.2]),
 ]:
     x = np.arange(len(y))
-    i2 = filters.f2_xy(y, x)
+    i2 = filters.F2_xy(y, x)
     plt.plot(x, y, "o-", label="all")
     plt.plot(x[:i2], y[:i2], ".-", label="filtered")
-    plt.title("f2")
+    plt.title("F2")
     plt.legend()
     plt.show()
 
